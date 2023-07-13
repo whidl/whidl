900c8ea3199e95b65a8a956ea5c5f62df85ccb0e
Daniel DeFreez <defreez@users.noreply.github.com>
2023-07-13 20:16:03 +0000

    Rest of the lib tags'

--- src/logisim.rs
+++ src/logisim.rs
@@ -17,7 +17,36 @@ struct Project {
     source: String,
     #[serde(rename = "@version")]
     version: String,
+    lib: Vec<Library>,
     circuit: Circuit,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "lib")]
+struct Library {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@desc")]
+    desc: Option<String>,
+    #[serde(rename = "tool")]
+    tools: Vec<Tool>,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "tool")]
+struct Tool {
+    #[serde(rename = "@name")]
+    name: String,
+    a: Vec<Attribute>,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "a")]
+struct Attribute {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@val")]
+    val: String,
 }
 
 #[derive(Serialize)]
@@ -75,6 +104,93 @@ struct Wire {
 // ========= CONVERSIONS ========== //
 impl From<&Chip> for Project {
     fn from(chip: &Chip) -> Project {
+        // <lib desc="#Wiring" name="0">
+        //     <tool name="Pin">
+        //     <a name="appearance" val="classic"/>
+        //     </tool>
+        // </lib>
+        let lib_0_wiring = Library {
+            desc: Some(String::from("#Wiring")),
+            name: String::from("0"),
+            tools: vec![Tool {
+                name: String::from("Pin"),
+                a: vec![Attribute {
+                    name: String::from("appearance"),
+                    val: String::from("classic"),
+                }],
+            }],
+        };
+        let lib_1_gates = Library {
+            name: String::from("1"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_2_plexers = Library {
+            name: String::from("2"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_3_arithmetic = Library {
+            name: String::from("3"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_4_memory = Library {
+            name: String::from("4"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_5_io = Library {
+            name: String::from("5"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_6_ttl = Library {
+            name: String::from("6"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_7_tcl = Library {
+            name: String::from("7"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_8_base = Library {
+            name: String::from("8"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_9_bfh = Library {
+            name: String::from("9"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_10_ioextra = Library {
+            name: String::from("10"),
+            tools: Vec::new(),
+            desc: None,
+        };
+        let lib_11_soc = Library {
+            name: String::from("11"),
+            tools: Vec::new(),
+            desc: None,
+        };
+
+        let lib = vec![
+            lib_0_wiring,
+            lib_1_gates,
+            lib_2_plexers,
+            lib_3_arithmetic,
+            lib_4_memory,
+            lib_5_io,
+            lib_6_ttl,
+            lib_7_tcl,
+            lib_8_base,
+            lib_9_bfh,
+            lib_10_ioextra,
+            lib_11_soc,
+        ];
+
         let mut circuit = Circuit {
             name: chip.name.clone(),
             components: Vec::new(),
@@ -89,6 +205,7 @@ impl From<&Chip> for Project {
             source: String::from("3.8.0"),
             version: String::from("1.0"),
             circuit,
+            lib,
         }
     }
 }
